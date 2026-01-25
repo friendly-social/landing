7a0b8eaa911b3277699baa075474af3fdf80c96a
y9san9 / Alex Sokol <y9san9@gmail.com>
2026-01-26 01:32:06 +0300

    feat: added other languages that are redirected to russian translation of this page

--- src/index.locale.ts
+++ src/index.locale.ts
@@ -1,13 +1,21 @@
 export type LocaleCode = "en" | "ru";
 
 export function getLocaleCode(): LocaleCode {
-    const locale = new Intl.Locale(navigator.language);
-    switch (locale.language) {
-        case "en":
-            return "en";
-        case "ru":
-            return "ru";
-        default:
-            return "en";
+    for (const language of navigator.languages) {
+        const locale = new Intl.Locale(language);
+        switch (locale.language) {
+            // In case if english is present and it's priority is higher
+            case "en":
+                return "en"
+            case "az": // Azerbaijani
+            case "kk": // Kazakh
+            case "tg": // Tajik
+            case "ky": // Kyrgyz
+            case "be": // Belarusian
+            case "uk": // Ukrainian
+            case "ru": // Russian
+                return "ru";
+        }
     }
+    return "en";
 }
